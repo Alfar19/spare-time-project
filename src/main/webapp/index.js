f456fbbaae1eb4fb7c87326952f8cffc40acfd42
alessio.faraone <a.faraone@crif.com>
2022-09-07 17:18:55 +0200

    develop different boxes

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,3 +1,17 @@
-function showDiv(element){
-    console.log(element)
+function toggleDiv(){
+    var mainContainerClasses = document.querySelector('.mainContainer').classList
+    if(mainContainerClasses.contains('slideLeft')){
+        mainContainerClasses.add('slideRight')
+        mainContainerClasses.remove('slideLeft');
+    }else {
+        mainContainerClasses.add('slideLeft')
+        mainContainerClasses.remove('slideRight');
+    }
+
+
+    var flipCards = document.querySelectorAll('.flip-card');
+    flipCards.forEach(el =>{
+        // el.parentElement.parentElement.style.justifyContent='flex-start'
+        // el.classList.add('slide')
+    })
 }
